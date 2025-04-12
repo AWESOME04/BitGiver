c7fbfc937a7a99c5f52c5eb03ba08c08acde2899
Evans Acheampong <evansachie01@gmail.com>
2025-04-12 11:55:45 +0000

    initial commit for frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-purple-200 to-pink-200 flex flex-col items-center justify-center p-6">
+      <h1 className="text-5xl font-bold text-gray-800 mb-4">
+        BitGiver
+      </h1>
+    </div>
+  );
+}
+
+export default App;
